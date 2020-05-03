aaf86d2bd6df2790dd4d39ef1d997123f3f23be8
Erik Nielsen <erik@312development.com>
2020-05-03 08:27:05 -0500

    added logging

--- config/jest/setupAfterEnv.js
+++ config/jest/setupAfterEnv.js
@@ -1,1 +1,5 @@
 require('dotenv').config();
+
+process.env.ALPACA_PAPER_TRADING = true;
+process.env.ALPACA_API_KEY = process.env.TEST_ALPACA_API_KEY;
+process.env.ALPACA_API_SECRET = process.env.TEST_ALPACA_API_SECRET;
--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-const debug = require('debug')('stonks');
+require('dotenv').config();
+const logger = require('./lib/helpers/logger');
+const validateOptions = require('./lib/helpers/validateOptions');
 const tradableAssets = require('./lib/trade/assets');
 const trade = require('./lib/trade');
 const getAccount = require('./lib/account/getAccount');
 const cancelAllOrders = require('./lib/orders/cancelAllOrders');
 
+
 (async () => {
-  await cancelAllOrders();
-  const { buyingPower } = await getAccount();
-  const symbols = await tradableAssets(buyingPower);
-  const cashPerAsset = buyingPower / symbols.length;
-  symbols.forEach(async (symbol) => {
-    const output = await trade(symbol, cashPerAsset);
-    if (output) {
-      debug(`Trade results for ${symbol}:`);
-      debug(output);
-    }
-  });
+  logger.info(`Starting stonks at ${(new Date()).toISOString()}`);
+  validateOptions(process.env, ['ALPACA_API_KEY', 'ALPACA_API_SECRET']);
+  try {
+    await cancelAllOrders();
+    const { buyingPower } = await getAccount();
+    const symbols = await tradableAssets(buyingPower);
+    const cashPerAsset = buyingPower / symbols.length;
+    const trades = symbols.map((symbol) => trade(symbol, cashPerAsset));
+    await Promise.all(trades);
+  } catch (error) {
+    logger.error(error);
+  }
+  logger.info(`Finished stonks at ${(new Date()).toISOString()}`);
 })();
