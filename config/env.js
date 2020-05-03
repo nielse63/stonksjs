d69a35295a6caaa4c27e5a07b0329ed23643aa38
Erik Nielsen <erik@312development.com>
2020-05-03 12:13:29 -0500

    Release 1.0.0-alpha.0

--- config/env.js
+++ config/env.js
@@ -1,8 +1,5 @@
 require('dotenv').config();
-const validateOptions = require('../lib/helpers/validateOptions');
 
 if (process.env.ALPACA_PAPER_TRADING === undefined) {
   process.env.ALPACA_PAPER_TRADING = true;
 }
-
-validateOptions(process.env, ['ALPACA_API_KEY', 'ALPACA_API_SECRET']);
