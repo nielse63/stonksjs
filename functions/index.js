87061b722fe35ca625c5be9b6217ce905115ea11
Erik Nielsen <erik@312development.com>
2020-05-03 13:42:29 -0500

    created scheduled functions

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,28 @@
 const functions = require('firebase-functions');
+const isWeekend = require('date-fns/isWeekend');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.trade = functions.https.onRequest(async (request, response) => {
-  const stonks = require('stonksjs');
-  try {
-    await stonks();
-    response.send('Complete');
-  } catch (error) {
-    response.send(error).status(500);
-  }
-});
+exports.trade = functions.pubsub
+  .schedule('every 30 minutes from 09:30 to 15:30')
+  .timeZone('America/New_York')
+  .onRun(async () => {
+    if (isWeekend(new Date())) {
+      return null;
+    }
+    process.env.ALPACA_API_KEY = functions.config().alpaca.key;
+    process.env.ALPACA_API_SECRET = functions.config().alpaca.secret;
+
+    if (!process.env.ALPACA_API_KEY || !process.env.ALPACA_API_SECRET) {
+      throw new Error('env vars ALPACA_API_KEY or ALPACA_API_SECRET missing');
+    }
+
+    const stonks = require('stonksjs');
+    try {
+      await stonks();
+    } catch (error) {
+      console.error(error);
+    }
+    return null;
+  });
