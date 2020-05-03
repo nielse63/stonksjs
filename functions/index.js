a07cdeedae1f4c4ff05a05f2805d2e4a7a70cd0a
Erik Nielsen <erik@312development.com>
2020-05-03 12:46:21 -0500

    updated functions

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,14 @@
 const functions = require('firebase-functions');
-// const stonks = require('stonksjs');
-
-// process.env.ALPACA_API_KEY = 'PKWIRZ6KTYD6PINJUL7H';
-// process.env.ALPACA_API_SECRET = 'HbdZRt9KD0zo1rpo0ARgA5PzWPuhOzYkB5h7VIbA';
-// process.env.ALPACA_PAPER_TRADING = true;
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  // console.log(stonks);
-  response.send('Hello from Firebase!');
+exports.trade = functions.https.onRequest(async (request, response) => {
+  const stonks = require('stonksjs');
+  try {
+    await stonks();
+    response.send('Complete');
+  } catch (error) {
+    response.send(error).status(500);
+  }
 });
