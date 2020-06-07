fa5ceaa8072602d535a6dbdcf35d64ab91e4de11
Erik Nielsen <erik@312development.com>
2020-06-07 12:52:45 -0500

    moved new functionality into ema script

---
+++ test.js
@@ -0,0 +1,22 @@
+const ema = require('./lib/ema');
+const sma = require('./lib/sma');
+
+const args = process.argv.slice(2);
+const stategy = args
+  .find((arg) => arg.startsWith('--strategy='))
+  .replace('--strategy=', '');
+console.log({ args, stategy });
+
+(async () => {
+  switch (stategy) {
+    case 'sma':
+      await sma();
+      break;
+    case 'ema':
+      await ema();
+      break;
+    default:
+      console.warn('no strategy provided. use --strategy=[ema|sma|macd]');
+      break;
+  }
+})();
