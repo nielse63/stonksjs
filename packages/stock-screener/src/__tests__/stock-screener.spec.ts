9ee0e8ce19f39cf1e7e0e905a4f8bc0e82d2d3df
Erik Nielsen <erik@312development.com>
2023-07-04 12:10:10 -0500

    updated github actions

--- packages/stock-screener/src/__tests__/stock-screener.spec.ts
+++ packages/stock-screener/src/__tests__/stock-screener.spec.ts
@@ -4,12 +4,7 @@ describe('stock-screener', () => {
   describe('#getScreenerResults', () => {
     it('should be an async function that resolves to an array', async () => {
       const response = await getScreenerResults('HIGH_YIELD_DIVIDENDS');
-      console.log(response);
       expect(response).toBeArray();
     });
-
-    // it('should reject if an invalid filter is passed', async () => {
-    //   await expect(getScreenerResults('invalidFilter')).toReject();
-    // });
   });
 });
